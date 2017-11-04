94f6f736cdcf4f4091450e9afcad895ec8af4ebf
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2017-11-04 10:33:17 +0100

    Update addon_template_ui_panel.py

--- addon_template_ui_panel.py
+++ addon_template_ui_panel.py
@@ -1,68 +1,65 @@
 bl_info = {
-    "name": "My Script",
-    "author": "Hans Willem Gijzel",
-    "version": (1, 0),
-    "location": "View3D > Tools > Scripts",
-    "category": "Scripts",
-}
+    'name' : 'My Addon',
+    'author' : 'Hans Willem Gijzel',
+    'version' : (1, 0),
+    'blender' : (2, 79),
+    'location' : 'View 3D > Tools > My Addon',
+    'description' : 'Says hello',
+    'warning' : '',
+    'wiki_url' : '',
+    'category' : 'My Addon Category'
+    }
 
 
+#imports
 import bpy
 
 
-#------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-#the actual script to execute when the button is pressed
-def myScript():
-    print("Hello world!")
-
-
-#------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-#boiler plate
-class myScriptPanel(bpy.types.Panel):
-    bl_space_type = "VIEW_3D"
-    bl_region_type = "TOOLS"
-    bl_context = "objectmode"
-    bl_category = "My Scripts" #the name of the tab in the ui
-    bl_label = "Execute My Script" #label in interface
-
-
+#panel class
+class MyPanel(bpy.types.Panel):
+    
+    #panel attributes
+    """What does this panel do?"""
+    bl_label = 'My Panel Label'
+    bl_idname = 'tools_my_panel'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'TOOLS'
+    bl_category = 'My Addon'
+    
+    #draw loop
     def draw(self, context):
-        #button 01
-        TheCol = self.layout.column(align=True)
-        TheCol.operator("script.hello_world", text="Say hello") #the operator class to execute and the text on the button
-    #end draw
+        layout = self.layout
+        layout.label('My Label')
+        layout.operator('script.my_operator', text="Say Hello!")
 
 
-class MyScriptOperator(bpy.types.Operator):
-    """My Script that just says hello""" #blender will use this as a tooltip for menu items and buttons
-    bl_idname = "script.hello_world" #unique identifier for buttons and menu items to reference
-    bl_label = "My Script" #display name in the interface
+#operator class
+class MyOperator(bpy.types.Operator):
+    
+    #opetator attributes
+    """What does this operator do?"""
+    bl_label = 'My Operator'
+    bl_idname = 'script.my_operator'
     bl_options = {'REGISTER', 'UNDO'}
 
-
-    def invoke(self, context, event):
-        #The actual script to call
-        myScript()
-
-
+    #execute
+    def execute(self, context):
+        print('Hello world!')
+        
         return {'FINISHED'}
-
-
+    
+        
+#registration
 def register():
-    bpy.utils.register_class(MyScriptOperator)
-    bpy.utils.register_class(myScriptPanel)
-
+    bpy.utils.register_class(MyPanel)
+    bpy.utils.register_class(MyOperator)
+    
 
 def unregister():
-    bpy.utils.unregister_class(MyScriptOperator)
-    bpy.utils.unregister_class(myScriptPanel)
+    bpy.utils.register_class(MyPanel)
+    bpy.utils.register_class(MyOperator)
 
 
-#this allows you to run the script directly from blenders text editor
-#to test the addon without having to install it
-if __name__ == "__main__":
+#enable to test the addon by running this script
+if __name__ == '__main__':
     register()
